fd5eb865f65daf39dae67ec4cd515b9f33442c36
Alexel <umbasadoge@gmail.com>
2016-12-13 01:44:47 -0800

    Removed comments in DeleteTask.php

--- src/controller/DeleteTask.php
+++ src/controller/DeleteTask.php
@@ -1,9 +1,5 @@
 <?php
 
-//This function queries the selected database for todo items and their respective information.
-//The database is passed as a parameter.
-//The information from this function will be passed to todo/view/DisplayTask.php 
-
 function DeleteTask($db)
 {
     echo "Delete";
