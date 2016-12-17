2271cde63aeb68173202d59036e836b5c4bb4c37
Alexel <umbasadoge@gmail.com>
2016-12-17 00:39:02 -0800

    Actually Deleted RetrieveTask.php in src/controller/RetrieveTask.php

--- home/index.php
+++ home/index.php
@@ -4,7 +4,6 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL|E_STRICT);
 
 include_once("../src/model/db/DbConnect.php");
-include_once("../src/controller/RetrieveTask.php");
 include_once("../src/view/DisplayTask.php");
 
 $dbHandle = new DbConnect();
